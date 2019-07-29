7c351868096ea5b7fe0f6f5b5f0e354edfa01ab8
Célian Veyssière <celian-veyssiere@harvest.fr>
2019-07-29 11:17:32 +0200

    Update demo

--- dist/ng-select2/polyfills.js
+++ dist/ng-select2/polyfills.js
@@ -5496,8 +5496,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! c:\Data\sources-svn\pitch\ng-select2\src\polyfills.ts */"./src/polyfills.ts");
-module.exports = __webpack_require__(/*! c:\Data\sources-svn\pitch\ng-select2\node_modules\@angular-devkit\build-angular\src\angular-cli-files\models\jit-polyfills.js */"./node_modules/@angular-devkit/build-angular/src/angular-cli-files/models/jit-polyfills.js");
+__webpack_require__(/*! C:\Data\sources-svn\pitch\ng-select2\src\polyfills.ts */"./src/polyfills.ts");
+module.exports = __webpack_require__(/*! C:\Data\sources-svn\pitch\ng-select2\node_modules\@angular-devkit\build-angular\src\angular-cli-files\models\jit-polyfills.js */"./node_modules/@angular-devkit/build-angular/src/angular-cli-files/models/jit-polyfills.js");
 
 
 /***/ })
--- dist/ng-select2/styles.js
+++ dist/ng-select2/styles.js
@@ -557,7 +557,7 @@ if(false) {}
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-module.exports = __webpack_require__(/*! c:\Data\sources-svn\pitch\ng-select2\src\styles.css */"./src/styles.css");
+module.exports = __webpack_require__(/*! C:\Data\sources-svn\pitch\ng-select2\src\styles.css */"./src/styles.css");
 
 
 /***/ })
