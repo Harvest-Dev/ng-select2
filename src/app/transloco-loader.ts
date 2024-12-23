53e6032284ebde5d963fc118ccdaa9b7e3cbc983
Zéfling <zefling@ikilote.net>
2024-12-23 12:45:32 +0100

    Fix demo

--- src/app/transloco-loader.ts
+++ src/app/transloco-loader.ts
@@ -1,12 +1,13 @@
-import { inject, Injectable } from "@angular/core";
-import { Translation, TranslocoLoader } from "@jsverse/transloco";
-import { HttpClient } from "@angular/common/http";
+import { HttpClient } from '@angular/common/http';
+import { Injectable, inject } from '@angular/core';
+
+import { Translation, TranslocoLoader } from '@jsverse/transloco';
 
 @Injectable({ providedIn: 'root' })
 export class TranslocoHttpLoader implements TranslocoLoader {
     private http = inject(HttpClient);
 
     getTranslation(lang: string) {
-        return this.http.get<Translation>(`/assets/i18n/${lang}.json`);
+        return this.http.get<Translation>(`./assets/i18n/${lang}.json`);
     }
 }
--- transloco.config.ts
+++ transloco.config.ts
@@ -1,9 +1,9 @@
-import {TranslocoGlobalConfig} from '@jsverse/transloco-utils';
-    
+import { TranslocoGlobalConfig } from '@jsverse/transloco-utils';
+
 const config: TranslocoGlobalConfig = {
-  rootTranslationsPath: 'src/assets/i18n/',
-  langs: [ 'en', 'fr' ],
-  keysManager: {}
+    rootTranslationsPath: '/src/assets/i18n/',
+    langs: ['en', 'fr'],
+    keysManager: {},
 };
-    
+
 export default config;
