5b3e4f0f02320b8dd8aafe03906f82273c65b008
Zéfling <zefling@ikilote.net>
2024-12-24 03:06:46 +0100

    Fix demo: other examples

--- dist/ng-select2/browser/chunk-KMZYN3XS.js
+++
@@ -1,1 +0,0 @@
-import{a,b}from"./chunk-52KL7BSV.js";import"./chunk-33O7KYOH.js";import"./chunk-PBM5R43U.js";import"./chunk-S5UJW4WF.js";import"./chunk-EQDQRRRY.js";export{b as ExamplesModule,a as ExamplesRoutingModule};
